4112e27864b1309244ef3cb6bfa4dc20982adedc
sso <93847254+moolew@users.noreply.github.com>
2026-01-07 01:53:09 +0000

    volume slider

---
+++ Assets/Scripts/Management/VolumeManager.cs
@@ -0,0 +1,63 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeManager : MonoBehaviour
+{
+    [SerializeField] private List<Image> volumeSprites = new();
+    [SerializeField] private float visibleTime = 2f;
+    private Transform _slider;
+
+    private float timer;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        _slider = transform.GetChild(0);
+    }
+
+    private bool _changed = false;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Equals))
+        {
+            AudioListener.volume += 0.1f;
+            timer = visibleTime;
+            _changed = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Minus))
+        {
+            AudioListener.volume -= 0.1f;
+            timer = visibleTime;
+            _changed = true;
+        }
+
+        AudioListener.volume = Mathf.Clamp01(AudioListener.volume);
+
+        if (_changed)
+            _slider.transform.localPosition = Vector3.Lerp(_slider.transform.localPosition, new(0, 520, 0), Time.deltaTime * 15f);
+        else
+            _slider.transform.localPosition = Vector3.Lerp(_slider.transform.localPosition, new(0, 700, 0), Time.deltaTime * 15f);
+
+        if (timer > 0.01f)
+        {
+            timer -= Time.deltaTime;
+        }
+
+        if (timer <= 0.01f)
+        {
+            _changed = false;
+        }
+    }
+
+    private void VolumeVisualiser(float volume)
+    {
+        int bars = Mathf.RoundToInt(volume * volumeSprites.Count);
+
+        for (int i = 0; i < volumeSprites.Count; i++)
+        {
+            volumeSprites[i].enabled = i < bars;
+        }
+    }
+}
